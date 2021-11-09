f80d73af44678d96870cfb690d861f92ed6e0c40
“dinels” <dinels12@gmail.com>
2021-11-08 21:32:15 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,54 @@
-#! /usr/bin/env node
-import shell from 'shelljs';
+#!/usr/bin/env node
 
-shell.exec('echo shell.exec works');
+import inquirer from "inquirer";
+import shell from "shelljs";
+
+const handleConfig = async () => {
+  const finded = shell.find("./desition.config.json")[0];
+  const regex = /desition|config|json/;
+
+  if (regex.test(finded)) console.log(finded);
+};
+
+handleConfig();
+
+enum Commands {
+  Create = "Create new api module",
+  Quit = "Quit",
+}
+
+async function promptCreate(): Promise<void> {
+  console.clear();
+  const answers = (await inquirer.prompt({
+    type: "input",
+    name: "create",
+    message: "Enter Api Name:",
+  })) as any;
+  if (answers["create"] !== "") {
+    const name = answers["create"];
+
+    // shell.mkdir(name);
+
+    console.log(answers["create"]);
+  }
+  promptUser();
+}
+
+async function promptUser(): Promise<void> {
+  // console.clear();
+  const answers = (await inquirer.prompt({
+    type: "list",
+    name: "command",
+    message: "Choose option",
+    choices: Object.values(Commands),
+  })) as any;
+  switch (answers["command"]) {
+    case Commands.Create:
+      promptCreate();
+      break;
+    case Commands.Quit:
+      break;
+  }
+}
+
+promptUser();
